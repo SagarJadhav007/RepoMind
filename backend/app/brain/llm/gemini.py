6bf56e11d343ef1a23c186f65b4807e626c82f97
Sagar Jadhav <jadhavsagar1505@gmail.com>
2026-01-10 11:24:04 +0530

    logging models

--- backend/app/brain/llm/gemini.py
+++ backend/app/brain/llm/gemini.py
@@ -13,8 +13,13 @@ class GeminiLLM:
         return result.embeddings[0].values
 
     def generate(self, prompt: str) -> str:
+        list_my_models()
         response = client.models.generate_content(
-            model="models/gemini-1.5-flash-001",
+            model="gemini-1.5-flash-latest", 
             contents=prompt,
         )
         return response.text
+    
+    def list_my_models():
+        for model in client.models.list():
+            print(f"Model Name: {model.name} - Supported Actions: {model.supported_actions}")
